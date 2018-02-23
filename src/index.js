1c7e7e5a6e1a4a503dfb95bb3d63c8eb7cdfa497
Luciano Viana Costa <luciano@weareadaptive.com>
2018-02-23 16:36:22 +0100

    testing

--- src/index.js
+++ src/index.js
@@ -95,7 +95,7 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const history = this.state.history.slice();
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
